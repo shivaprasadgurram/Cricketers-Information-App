724534678dc21e3d59fbb2c4d44cc8d03a6b2cc4
Shiva Prasad Gurram <gurramshiva05@gmail.com>
2021-06-29 08:27:23 +0530

    Update CricketerService.js

--- src/services/CricketerService.js
+++ src/services/CricketerService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const CRICKETER_API_BASE_URL =
-  'https://cricketers-info-sp.herokuapp.com/api/vi/cricketers'
+  'https://cricketers-info-sp.herokuapp.com/api/v1/cricketers'
 class CricketerService {
   getCricketers() {
     return axios.get(CRICKETER_API_BASE_URL)
